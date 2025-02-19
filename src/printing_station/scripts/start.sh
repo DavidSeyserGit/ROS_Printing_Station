3124eff43bc5b4e2b7f414cb7c0c91d23057be15
DavidSeyserGit <96821053+DavidSeyserGit@users.noreply.github.com>
2025-02-19 17:20:31 +0100

    changed the startup script

--- src/printing_station/scripts/start.sh
+++ src/printing_station/scripts/start.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Function to clean up background processes
+cleanup() {
+    echo "Stopping processes..."
+    kill $ROSLAUNCH_PID
+    kill $GUI_PID
+    exit 0
+}
+
+# Trap the EXIT signal to ensure cleanup is called
+trap cleanup EXIT
+
 roslaunch printing_station station.launch &
+ROSLAUNCH_PID=$!
 
-sleep 5
+# Check if PROJECT_ROOT is already set in the environment
+if [[ -z "$PROJECT_ROOT" ]]; then
+    echo "Error: PROJECT_ROOT environment variable not set."
+    echo "Please set PROJECT_ROOT to the absolute path of your project."
+    exit 1
+fi
 
-# Navigate to the external directory
-cd ../../external/3D-Printing-Station-GUI/target/debug
+EXECUTABLE_PATH=$(realpath "$PROJECT_ROOT/external/3D-Printing-Station-GUI/target/debug/")
 
+cd "$EXECUTABLE_PATH"
 ./GUI-3D-Printing-Station &
+GUI_PID=$!
+
+# Wait for background processes to finish
+wait $ROSLAUNCH_PID
+wait $GUI_PID

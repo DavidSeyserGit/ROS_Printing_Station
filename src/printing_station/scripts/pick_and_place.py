07d583c675717c8e2c59f0b44cddbac865e0d14a
DavidSeyserGit <david.seyser@hotmail.com>
2025-04-30 20:49:46 +0200

    made a few target poses

--- src/printing_station/scripts/pick_and_place.py
+++ src/printing_station/scripts/pick_and_place.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import threading
 import rospy
 import moveit_commander
 from geometry_msgs.msg import Pose
@@ -8,7 +7,10 @@ from moveit_commander import PlanningSceneInterface
 from moveit_msgs.msg import AttachedCollisionObject
 from gazebo_msgs.msg import ContactsState
 from RobotMover import RobotMove, run_robot
+import random
 
+traget_pose_list = [-1,3,1.2, 0, 0, 0, 1]
+drop_off = [-0.4, 2.2, 0.9, 0, -0.707, 0, 0.707]  # Quaternion for z-axis pointing to -y
 
 def spawn_object(model_path, model_name, initial_pose):
     """
@@ -48,66 +50,59 @@ def main():
     # Initialize planning scene
     planning_scene = PlanningSceneInterface()
 
-    # 1. Spawn the object in Gazebo (Uncomment if you need to spawn)
+    #should spawn randomly at a pre determined place (lets say 1 out of 5)
     model_file = "src/printing_station/urdf/morobot.sdf"
     model_name = "object_model"
     target_pose = Pose()
     target_pose.position.x = -1
     target_pose.position.y = 3
-    target_pose.position.z = 2
+    target_pose.position.z = 1
     target_pose.orientation.w = 1.0
 
     try:
         spawn_success = spawn_object(model_file, model_name, target_pose)
     except:
         pass
-
+    #need to have 5 targets where i can move to
     rospy.sleep(1.0)
-
-    robot_link = "scara_link3" 
-    global contact_sub
-    contact_sub = rospy.Subscriber(
-        '/gazebo/contact_states',
-        ContactsState,
-        callback=contact_callback,
-        callback_args=(robot_link, model_name, planning_scene)
-    )
-
     try:
         move_group = moveit_commander.MoveGroupCommander("knick")
         move_group.set_planning_time(10.0) # Increase planning time if needed
         move_group.set_num_planning_attempts(5)
         move_group.set_goal_joint_tolerance(0.01) # Set tolerance for joint goals
 
-        move_group.set_position_target(target_pose)
-
-
-        #need to find a way to make this dynamic for each place the object spawns
-        """
-        target_joint_waypoints = [
-            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],   
-            [0.5, -0.3, 0.8, -1.2, 0.0, 0.0],   
-            [-0.2, 0.1, 0.5, -0.6, 0.0, 0.0],   
-            [0.7, -0.5, 1.0, -1.5, 0.0, 0.0],    
-            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]      
-        ]
-        for i, target_joints in enumerate(target_joint_waypoints):
-            move_group.set_joint_value_target(target_joints)
-
-            rospy.loginfo(f"Planning motion to waypoint {i+1}")
-            plan_result = move_group.plan()
-            success = plan_result[0]
-            plan = plan_result[1]
-
-            if success:
+        #i have to move to the position and then move to the drop off location
+        move_group.set_position_target(traget_pose_list)
+        plan_result = move_group.plan()
+        success = plan_result[0]
+        plan = plan_result[1]
+        
+        move_group.execute(plan, wait=True)
+        if success:
                 move_group.execute(plan, wait=True)
         
-            else:
-                rospy.logerr(f"Failed to plan motion to waypoint {i+1}.")
-                break
+        else:
+            rospy.logerr(f"Failed to plan motion.")
+            
+        move_group = moveit_commander.MoveGroupCommander("knick")
+        move_group.set_planning_time(10.0) # Increase planning time if needed
+        move_group.set_num_planning_attempts(5)
+        move_group.set_goal_joint_tolerance(0.01) # Set tolerance for joint goals
 
-        rospy.spin()
-        """
+        #i have to move to the position and then move to the drop off location
+        move_group.set_position_target(drop_off)
+        plan_result = move_group.plan()
+        success = plan_result[0]
+        plan = plan_result[1]
+        
+        move_group.execute(plan, wait=True)
+        if success:
+                move_group.execute(plan, wait=True)
+        
+        else:
+            rospy.logerr(f"Failed to plan motion.")
+
+
     except Exception as e:
         rospy.logerr(f"Error in MoveGroup execution or planning: {e}")
         # If an exception occurs, ensure MoveIt commander is shut down

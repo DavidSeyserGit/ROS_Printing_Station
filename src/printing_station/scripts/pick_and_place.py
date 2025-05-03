db3a95d08ed580a7ecb23c3f9c2ac3f08d9be847
DavidSeyserGit <david.seyser@hotmail.com>
2025-05-03 09:01:41 +0200

    added multiple targets

--- src/printing_station/scripts/pick_and_place.py
+++ src/printing_station/scripts/pick_and_place.py
@@ -2,15 +2,21 @@
 import rospy
 import moveit_commander
 from geometry_msgs.msg import Pose
-from gazebo_msgs.srv import SpawnModel, SpawnModelRequest
+from gazebo_msgs.srv import SpawnModel, SpawnModelRequest, DeleteModel
 from moveit_commander import PlanningSceneInterface
 from moveit_msgs.msg import AttachedCollisionObject
 from gazebo_msgs.msg import ContactsState
 from RobotMover import RobotMove, run_robot
 import random
 
-traget_pose_list = [-1,3,1.2, 0, 0, 0, 1]
-drop_off = [-0.4, 2.2, 0.9, 0, -0.707, 0, 0.707]  # Quaternion for z-axis pointing to -y
+traget_pose_1 = [-1, 3, 1.7 , 0, 0, 0, 1]
+traget_pose_2 = [ 0.3, 3, 1.7 , 0, 0, 0, 1]
+traget_pose_3 = [ 0.3, 2.5, 1.7, 0, 0, 0, 1]
+traget_pose_4 = [ -1, 2.5, 1.7, 0, 0, 0, 1]
+traget_pose_5 = [-0.5, 3.5, 1.7, 0, 0, 0, 1]
+drop_off = [-0.4, 2.2, 0.9, 0, -0.707, 0, 0.707]  # Quaternion for z-axis pointing to -y (doesnt work for what ever reason)
+
+target_index = random.randint(1, 5)
 
 def spawn_object(model_path, model_name, initial_pose):
     """
@@ -28,8 +34,7 @@ def spawn_object(model_path, model_name, initial_pose):
         req.model_xml = model_xml
         req.initial_pose = initial_pose
         req.reference_frame = "world"
-
-        rospy.loginfo(f"Spawning {model_name} at position: {initial_pose.position}")
+        
         response = spawn_model(req.model_name, req.model_xml, "", 
                                req.initial_pose, req.reference_frame)
         if response.success:
@@ -44,63 +49,76 @@ def spawn_object(model_path, model_name, initial_pose):
 
 
 def main():
+    print (target_index)
     rospy.init_node("gazebo_moveit_waypoints", anonymous=True)
     moveit_commander.roscpp_initialize([])
-
     # Initialize planning scene
     planning_scene = PlanningSceneInterface()
 
-    #should spawn randomly at a pre determined place (lets say 1 out of 5)
+
+    # Spawn the object at one of the target locations randomly
     model_file = "src/printing_station/urdf/morobot.sdf"
     model_name = "object_model"
     target_pose = Pose()
-    target_pose.position.x = -1
-    target_pose.position.y = 3
-    target_pose.position.z = 1
-    target_pose.orientation.w = 1.0
+    selected_target_pose = globals()[f"traget_pose_{target_index}"]
+
+    # Assign the selected target pose to the spawn position
+    target_pose.position.x = selected_target_pose[0]
+    target_pose.position.y = selected_target_pose[1]
+    target_pose.position.z = selected_target_pose[2]-0.5
+    target_pose.orientation.w = 1
 
     try:
         spawn_success = spawn_object(model_file, model_name, target_pose)
     except:
         pass
+    
     #need to have 5 targets where i can move to
     rospy.sleep(1.0)
     try:
         move_group = moveit_commander.MoveGroupCommander("knick")
-        move_group.set_planning_time(10.0) # Increase planning time if needed
-        move_group.set_num_planning_attempts(5)
-        move_group.set_goal_joint_tolerance(0.01) # Set tolerance for joint goals
-
+        move_group.set_planning_time(5.0) # Increase planning time if needed
+        
         #i have to move to the position and then move to the drop off location
-        move_group.set_position_target(traget_pose_list)
+        move_group.set_pose_target(selected_target_pose)
         plan_result = move_group.plan()
         success = plan_result[0]
         plan = plan_result[1]
         
-        move_group.execute(plan, wait=True)
         if success:
                 move_group.execute(plan, wait=True)
         
         else:
             rospy.logerr(f"Failed to plan motion.")
             
-        move_group = moveit_commander.MoveGroupCommander("knick")
-        move_group.set_planning_time(10.0) # Increase planning time if needed
-        move_group.set_num_planning_attempts(5)
-        move_group.set_goal_joint_tolerance(0.01) # Set tolerance for joint goals
-
         #i have to move to the position and then move to the drop off location
-        move_group.set_position_target(drop_off)
+        move_group.set_pose_target(drop_off)
         plan_result = move_group.plan()
         success = plan_result[0]
         plan = plan_result[1]
         
-        move_group.execute(plan, wait=True)
         if success:
                 move_group.execute(plan, wait=True)
         
         else:
             rospy.logerr(f"Failed to plan motion.")
+
+        scara_waypoints_file = "src/printing_station/waypoints/scara_wp.json"
+        scara_robot = RobotMove(
+            robot_name="scara",
+            move_group_name="scara",
+            waypoints_file=scara_waypoints_file,
+        )
+
+        # Run the robot motion in the main thread since we don't need to do anything else
+        run_robot(scara_robot)
+
+        try:
+            delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+            delete_model(model_name)
+            rospy.loginfo(f"Successfully despawned {model_name}.")
+        except rospy.ServiceException as e:
+            rospy.logerr(f"Service call to delete model failed: {e}")
 
 
     except Exception as e:
@@ -119,13 +137,4 @@ if __name__ == "__main__":
         moveit_commander.roscpp_shutdown()
 
     """
-    scara_waypoints_file = "src/printing_station/waypoints/knick_wp.json"
-        scara_robot = RobotMove(
-            robot_name="knick",
-            move_group_name="knick",
-            waypoints_file=scara_waypoints_file,
-        )
-
-        # Run the robot motion in the main thread since we don't need to do anything else
-        run_robot(scara_robot)
     """
